b76a413b5847b202db4d3a66b90cc0802c9b4fee
unknown <lauriegale10@gmail.com>
2025-10-18 11:25:44 +0100

    Implement initial homepage design

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
 import { ChallengeDashboardComponent } from './challenge-dashboard/challenge-dashboard.component';
 import { PrimmDebugViewComponent } from './primm-debug-view/primm-debug-view.component';
 
 export const routes: Routes = [
-    {path:'', component: ChallengeDashboardComponent},
+    {path:'', component: HomepageComponent},
+    {path: 'dashboard', component: ChallengeDashboardComponent},
     {path:'exercise', component: PrimmDebugViewComponent}
 ];
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToolbarComponent } from '../toolbar/toolbar.component';
+
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-homepage',
+  standalone: true,
+  imports: [ToolbarComponent, MatButtonModule],
+  templateUrl: './homepage.component.html',
+  styleUrl: './homepage.component.sass'
+})
+export class HomepageComponent {
+
+  constructor(private router: Router) { }
+
+  toChallengeDashboard() {
+    let route = '/dashboard';
+    this.router.navigate([route]);
+  }
+
+  toTeacherInfo() {
+    let route = '/teacher-info';
+  }
+}
